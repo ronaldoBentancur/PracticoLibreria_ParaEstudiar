10135e48c5f5c86304c02872884550acc82a616d
Ronaldo <rb269475@fi365.ort.edu.uy>
2026-04-16 15:43:43 -0300

    Codigo de hasta la fecha 16/4/26

---
+++ LogicaAccesoDatos/EF/LibreriaContext.cs
@@ -0,0 +1,18 @@
+﻿using LogicaNegocio.ClasesDominio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicaAccesoDatos.EF
+{
+    // Ingrediente 1: Clase que hereda de DbContext
+    public class LibreriaContext : DbContext 
+    {
+        // Ingrediente 2: Tantos DbSets como entidades quiero que EF maneje (mapee) 
+        public DbSet<Tema> Temas { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        // Ingrediente 3: Configurar la conexión a la BD
+    }
+}
---
+++ LogicaNegocio/ClasesDominio/Rol.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicaNegocio.ClasesDominio
+{
+    public class Rol
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
